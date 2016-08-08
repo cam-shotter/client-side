6a623da312a2f7ab344395657c5072313353e711
Max Robinson & Cameron Shotter <pair+Maxwell-Robinson.cam-shotter@devbootcamp.com>
2016-08-09 11:22:26 +1200

    Updates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ xhr.get(endpoint, function (err, data) {
 
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
-  var iss = data.body
+  var iss = data.body.name
 
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
